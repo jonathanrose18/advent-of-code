f4eba271cd6c5e13310ac982237cce73a6f2b30a
Jonathan Rose <jonathan.rose@techstube.io>
2025-12-06 22:17:31 +0100

    solved day-1

---
+++ 2025/day-1/part-2/main.py
@@ -0,0 +1,24 @@
+dial = 50
+counter = 0
+
+with open("./input.txt", "r", encoding="utf-8") as f:
+    for line in f:
+        direction = line[0]
+        distance = int(line[1:])
+        if direction == "R":
+            for i in range(distance):
+                if dial == 99:
+                    dial = 0
+                    counter = counter + 1
+                else:
+                    dial = dial + 1
+        else:
+            for i in range(distance):
+                if dial == 0:
+                    dial = 99
+                elif dial == 1:
+                    dial = 0
+                    counter = counter + 1
+                else:
+                    dial = dial -1
+print(counter) 
